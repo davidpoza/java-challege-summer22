bcd0f3248942179436b3c0c27c4f56c814dbb936
David Poza Suárez <david.poza-suarez@t-systems.com>
2022-07-04 21:52:40 +0200

    added sqlite driver

--- whey-pricing/src/com/davidpoza/WheyPricing.java
+++ whey-pricing/src/com/davidpoza/WheyPricing.java
@@ -4,6 +4,8 @@ package com.davidpoza;
 public class WheyPricing {
 
   public static void main(String[] args) throws Exception {
+    Connect.connect();
+    
     MyProteinPrice p1 = new MyProteinPrice("nutricion-deportiva/impact-whey-protein/10530943.html?variation=12309348", 5.0);
     p1.scrapPrice();
     
