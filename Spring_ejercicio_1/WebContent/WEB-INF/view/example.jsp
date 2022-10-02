1419f115cb39a916665c22b73be557785bf858de
David Poza Suárez <david.poza-suarez@t-systems.com>
2022-10-02 15:10:03 +0200

    changes in example

--- Spring_ejercicio_1/WebContent/WEB-INF/view/example.jsp
+++ Spring_ejercicio_1/WebContent/WEB-INF/view/example.jsp
@@ -5,9 +5,11 @@
 <head>
 <meta charset="UTF-8">
 <title>Hello world</title>
+<link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/resources/css/styles.css" />
 </head>
 <body>
 <h1>HELLO WORLD</h1>
-<a href="form">Muestra form</a>
+<a href="form">Muestra form</a><br />
+<a href="form2">Muestra form2</a>
 </body>
 </html>
--- Spring_ejercicio_1/src/main/java/com/davidpoza/ControllerExample.java
+++ Spring_ejercicio_1/src/main/java/com/davidpoza/ControllerExample.java
@@ -1,5 +1,8 @@
 package com.davidpoza;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +20,18 @@ public class ControllerExample {
   @RequestMapping("/process-form")
   public String showResult() {
     return "result";
+  }
+  
+  @RequestMapping("/form2")
+  public String showForm2() {
+    return "form2";
+  }
+  
+  @RequestMapping("/process-form2")
+  public String showResult2(HttpServletRequest request, Model model) {
+    String name = request.getParameter("name");
+    String upperCaseName = name.toUpperCase();
+    model.addAttribute("upperCaseName", upperCaseName);
+    return "result2";
   }
 }
